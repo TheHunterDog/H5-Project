9bd00444c5cfdc8ad3a54dbba88814339e437500
Mark Heijnekamp <mark@heijnekamp.nl>
2022-04-25 19:33:59 +0200

    Add Generic repository

---
+++ WPF/Interfaces/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace WPF.Interfaces;
+
+public interface IGenericRepository<T> where T : class
+{
+    T? GetById(int id);
+    IEnumerable<T> GetAll();
+    IEnumerable<T> Find(Expression<Func<T, bool>> expression);
+    void Add(T entity);
+    void AddRange(IEnumerable<T> entities);
+    void Remove(T entity);
+    void Remove(int id);
+    void Update(T entity);
+    void RemoveRange(IEnumerable<T> entities);
+    void Save();
+}
