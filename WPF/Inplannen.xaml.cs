b0e439acb18d7c1cc8ddf0270f008b543a125dac
LAPTOP-UFABUB1N\trist <s1147577@windesheim.nl>
2022-04-26 12:12:04 +0200

    added StudentBegeleiderGesprekken insertion into database

--- WPF/Inplannen.xaml.cs
+++ WPF/Inplannen.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Shapes;
 using System.Diagnostics; // kan weg
 using System.Net.Mail;
 
+
 namespace WPF
 {
     /// <summary>
@@ -30,14 +31,45 @@ namespace WPF
         private void Submit_Click(object sender, RoutedEventArgs e)
         {
             MailMessage message = new MailMessage("afspraakplanner123@gmail.com", "s1147577@student.windesheim.nl", $"afspraak op {DatePicked.Text}", $"Er is een afspraak ingpepland voor {DatePicked.Text} met als opmerking:\n {opmerkingen.Text}");
-
             SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
             client.Credentials = new System.Net.NetworkCredential("afspraakplanner123@gmail.com", "Mailtest123");
             client.EnableSsl = true;
             DateTime datumAfspraak = (DateTime)DatePicked.SelectedDate;
             datumAfspraak = new DateTime(datumAfspraak.Year, datumAfspraak.Month, datumAfspraak.Day, Int16.Parse(Hours.Text), Int16.Parse(Minutes.Text), 0);
             Trace.WriteLine($"{datumAfspraak} + {opmerkingen.Text}");
-            client.Send(message);
+            //client.Send(message);
+            /*            Model.StudentBegeleiderGesprekken test = new Model.StudentBegeleiderGesprekken
+                        {
+                            GesprekDatum = datumAfspraak,
+                            StudentId = 1147577,
+                            StudentBegeleider = new Model.StudentBegeleider
+                            {
+                                Docentcode = "test123",
+                                Id = 123,
+                                Naam = "peter pottebakker",
+                                Students = new List<Model.Student>()
+                            },
+                            Opmerkingen = $"{opmerkingen.Text}",
+                            Student = new Model.Student
+                            {
+
+                            },
+                            Voltooid = false
+                        };*/
+            using (var contex = new Model.StudentBeleidContext())
+            {
+                contex.StudentBegeleiderGesprekken.Add(new Model.StudentBegeleiderGesprekken
+                {
+                    StudentId = 1,
+                    Student = contex.Students.Find(1),
+                    StudentBegeleiderId = 1,
+                    StudentBegeleider = contex.StudentBegeleiders.Find(1),
+                    GesprekDatum = datumAfspraak,
+                    Voltooid = false,
+                    Opmerkingen = $"{opmerkingen.Text}"
+                });
+                contex.SaveChanges();
+            }
             this.Close();
         }
     }
