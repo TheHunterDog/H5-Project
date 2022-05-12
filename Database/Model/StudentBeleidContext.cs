d4726670dcfb3cc4da030389332a08619527a1fe
Mark Heijnekamp <mark@heijnekamp.nl>
2022-05-12 13:11:10 +0200

    Data seeding

--- Database/Model/StudentBeleidContext.cs
+++ Database/Model/StudentBeleidContext.cs
@@ -32,71 +32,109 @@ public class StudentBeleidContext: DbContext
        // options.UseSqlServer(System.Configuration.ConfigurationManager.ConnectionStrings["StudentBeleidDatabase"].ConnectionString);
        options.UseSqlServer("Server=ftp.huttennl.nl,1433;Database=StudentBegeleid;User Id=sa;Password=9CknApvBHa2aNuovTirqhmEd");
     }
-    
+
     /// <summary>
     /// Set all fields to required constraints
     /// </summary>
     /// <param name="modelBuilder"></param>
     protected override void OnModelCreating(ModelBuilder modelBuilder)
-   {
+    {
 
-      #region Student
-      modelBuilder.Entity<Student>()
-         .HasKey(s => new {s.Id});
-      modelBuilder.Entity<Student>()
-         .Property(s => s.Id)
-         .ValueGeneratedOnAdd();
-      modelBuilder.Entity<Student>()
-         .HasIndex(s => s.Studentnummer).IsUnique();
-      modelBuilder.Entity<Student>()
-         .Property(s => s.Voornaam).IsRequired();
-      modelBuilder.Entity<Student>()
-      .Property(s => s.Tussenvoegsel).IsRequired(false);
-      modelBuilder.Entity<Student>()
-         .Property(s => s.Achternaam).IsRequired();
-      modelBuilder.Entity<Student>()
-         .Property(s => s.Klasscode).IsRequired();
+       #region Student
 
-
-      modelBuilder.Entity<Student>()
-         .HasOne(s => s.Studentbegeleider)
-         .WithMany(sb => sb.Students)
-         .HasForeignKey(s => s.StudentbegeleiderId)
-         .OnDelete(DeleteBehavior.NoAction)
-         .IsRequired(false);
-      
-
-      #endregion
-   
-      #region StudentBegeleider
-   
-      modelBuilder.Entity<StudentBegeleider>()
-         .HasKey(s => new {s.Id});
-      modelBuilder.Entity<StudentBegeleider>()
-         .HasIndex(s => s.Docentcode).IsUnique();
-      modelBuilder.Entity<StudentBegeleider>()
-         .Property(s => s.Docentcode).IsRequired();
+       modelBuilder.Entity<Student>()
+          .HasKey(s => new {s.Id});
+       modelBuilder.Entity<Student>()
+          .Property(s => s.Id)
+          .ValueGeneratedOnAdd();
+       modelBuilder.Entity<Student>()
+          .HasIndex(s => s.Studentnummer).IsUnique();
+       modelBuilder.Entity<Student>()
+          .Property(s => s.Voornaam).IsRequired();
+       modelBuilder.Entity<Student>()
+          .Property(s => s.Tussenvoegsel).IsRequired(false);
+       modelBuilder.Entity<Student>()
+          .Property(s => s.Achternaam).IsRequired();
+       modelBuilder.Entity<Student>()
+          .Property(s => s.Klasscode).IsRequired();
 
 
-      #endregion
-   
-      #region studentBegeleiderGesprekken
+       modelBuilder.Entity<Student>()
+          .HasOne(s => s.Studentbegeleider)
+          .WithMany(sb => sb.Students)
+          .HasForeignKey(s => s.StudentbegeleiderId)
+          .OnDelete(DeleteBehavior.NoAction)
+          .IsRequired(false);
 
-      modelBuilder.Entity<StudentBegeleiderGesprekken>()
-         .HasKey(sbg => new {sbg.StudentId, sbg.StudentBegeleiderId,sbg.GesprekDatum});
+       modelBuilder.Entity<Student>().HasData(
+          new Student
+          {
+             Klasscode = "OOSDDH2022", Voornaam = "Mark", Achternaam = "Heijnekamp", Tussenvoegsel = "",
+             Studentnummer = "s1156618",Id = 1156618
+          },
+          new Student
+          {
+             Klasscode = "OOSDDH2022", Voornaam = "Rob", Achternaam = "Hutten", Tussenvoegsel = "",
+             Studentnummer = "s1152882",Id = 1152882
+          }, new Student
+          {
+             Klasscode = "OOSDDH2022", Voornaam = "Antoine", Achternaam = "Pijlgroms", Tussenvoegsel = "",
+             Studentnummer = "s1159362",Id = 1159362
+          }, new Student
+          {
+             Klasscode = "OOSDDH2022", Voornaam = "Evert-Jan", Achternaam = "Nijsink", Tussenvoegsel = "",
+             Studentnummer = "s1160918",Id = 1160918
+          }, new Student
+          {
+             Klasscode = "OOSDDH2022", Voornaam = "Tristan", Achternaam = "Jongedijk", Tussenvoegsel = "",
+             Studentnummer = "s1147577",Id = 1147577
+          },
+          new Student
+          {
+             Klasscode = "OOSDDH2023", Voornaam = "Jan", Achternaam = "Jaap", Tussenvoegsel = "Jappie",
+             Studentnummer = "s1147576",Id = 1147576
+          }
+       );
 
-      modelBuilder.Entity<StudentBegeleiderGesprekken>()
-         .HasOne(sbg => sbg.Student)
-         .WithMany(s => s.StudentBegeleiderGesprekken)
-         .HasForeignKey(sbg => sbg.StudentId)
-         .OnDelete(DeleteBehavior.Cascade);
+       #endregion
 
-      modelBuilder.Entity<StudentBegeleiderGesprekken>()
-         .HasOne(sbg => sbg.StudentBegeleider)
-         .WithMany(sb => sb.StudentBegeleiderGesprekken)
-         .HasForeignKey(sbg => sbg.StudentBegeleiderId)
-         .OnDelete(DeleteBehavior.Cascade);
+       #region StudentBegeleider
 
-      #endregion
-   }
+       modelBuilder.Entity<StudentBegeleider>()
+          .HasKey(s => new {s.Id});
+       modelBuilder.Entity<StudentBegeleider>()
+          .HasIndex(s => s.Docentcode).IsUnique();
+       modelBuilder.Entity<StudentBegeleider>()
+          .Property(s => s.Docentcode).IsRequired();
+
+       modelBuilder.Entity<StudentBegeleider>().HasData(
+
+          new StudentBegeleider
+          {
+             Docentcode = "SA1234",Naam = "Karen brakband",Id = 964
+          }
+       );
+       
+       #endregion
+
+       #region studentBegeleiderGesprekken
+
+       modelBuilder.Entity<StudentBegeleiderGesprekken>()
+          .HasKey(sbg => new {sbg.StudentId, sbg.StudentBegeleiderId, sbg.GesprekDatum});
+
+       modelBuilder.Entity<StudentBegeleiderGesprekken>()
+          .HasOne(sbg => sbg.Student)
+          .WithMany(s => s.StudentBegeleiderGesprekken)
+          .HasForeignKey(sbg => sbg.StudentId)
+          .OnDelete(DeleteBehavior.Cascade);
+
+       modelBuilder.Entity<StudentBegeleiderGesprekken>()
+          .HasOne(sbg => sbg.StudentBegeleider)
+          .WithMany(sb => sb.StudentBegeleiderGesprekken)
+          .HasForeignKey(sbg => sbg.StudentBegeleiderId)
+          .OnDelete(DeleteBehavior.Cascade);
+
+       #endregion
+       
+    }
 }
