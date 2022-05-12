49f3a037de626f2230fd3d3377ea378608674716
Robpizza <12848142+Robpizza@users.noreply.github.com>
2022-05-12 22:05:56 +0200

    Added Studentproblem class. Added the association to the Teacher and Student

---
+++ WPF/Model/StudentProblem.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WPF.Model;
+
+public class Student
+{
+    #region PrivateFields
+    /// <summary>
+    /// Student has an id
+    /// </summary>
+    private int _id;
+    
+    /// <summary>
+    /// student has an studentnumber
+    /// </summary>
+    private string _studentnummer;
+    
+    /// <summary>
+    /// Student has a firstname
+    /// </summary>
+    private string _voornaam;
+    
+    /// <summary>
+    /// student might have a surname prefix
+    /// </summary>
+    private string _tussenvoegsel;
+    
+    /// <summary>
+    /// Student has a lastname
+    /// </summary>
+    private string _achternaam;
+    
+    /// <summary>
+    /// student has a class code
+    /// </summary>
+    private string _klasscode;
+    
+    /// <summary>
+    /// Student has a Studentbegeleider forgein-key
+    /// </summary>
+    private int _studentbegeleiderId;
+    /// <summary>
+    /// Student has a studentbegeleider
+    /// </summary>
+    private StudentBegeleider _studentBegeleider;
+    
+    /// <summary>
+    /// A student belongs to many meetings
+    /// </summary>
+    private IEnumerable<StudentBegeleiderGesprekken> _studentBegeleiderGesprekken;
+    
+    #endregion
+
+    #region Properties
+
+    public IEnumerable<StudentBegeleiderGesprekken> StudentBegeleiderGesprekken
+    {
+        get => _studentBegeleiderGesprekken;
+        set => _studentBegeleiderGesprekken = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+
+    public int Id
+    {
+        get => _id;
+        set => _id = value;
+    }
+
+    public string Studentnummer
+    {
+        get => _studentnummer;
+        set => _studentnummer = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string Voornaam
+    {
+        get => _voornaam;
+        set => _voornaam = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string Tussenvoegsel
+    {
+        get => _tussenvoegsel;
+        set => _tussenvoegsel = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string Achternaam
+    {
+        get => _achternaam;
+        set => _achternaam = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string Klasscode
+    {
+        get => _klasscode;
+        set => _klasscode = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public StudentBegeleider Studentbegeleider
+    {
+        get => _studentBegeleider;
+        set => _studentBegeleider = value;
+    }
+
+    public int StudentbegeleiderId
+    {
+        get => _studentbegeleiderId;
+        set => _studentbegeleiderId = value;
+    }
+
+    #endregion
+    
+}
---
+++ WPF/Model/Teacher.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WPF.Model;
+
+public class StudentProblem
+{
+    #region PrivateFields
+    /// <summary>
+    /// StudentProblem has an associated <see cref="Student"/>
+    /// </summary>
+    private int _studentId;
+    
+    /// <summary>
+    /// StudentProblem has an associated Teacher
+    /// </summary>
+    private int _teacherId;
+    
+    /// <summary>
+    /// StudentProblem has an priority
+    /// </summary>
+    private int _priority;
+    
+    /// <summary>
+    /// StudentProblem has an description
+    /// </summary>
+    private string _description;
+    
+    #endregion
+
+    #region Properties
+
+    public int Student
+    {
+        get => _studentId;
+        set => _studentId = value;
+    }
+    
+    public int Teacher
+    {
+        get => _teacherId;
+        set => _teacherId = value;
+    }
+    
+    public int Priority
+    {
+        get => _priority;
+        set => _priority = value;
+    }
+    
+    public string Description
+    {
+        get => _description;
+        set => _description = value;
+    }
+    
+    #endregion
+}
