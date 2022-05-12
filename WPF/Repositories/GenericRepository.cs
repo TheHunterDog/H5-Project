b9083d2011e3e85016e03f1fdbea3a859d17fb19
Robpizza <12848142+Robpizza@users.noreply.github.com>
2022-05-12 22:33:08 +0200

    Renamed the unittest project.

--- WPF/Repositories/GenericRepository.cs
+++ WPF/Repositories/GenericRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using Database.Model;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using WPF.Interfaces;
