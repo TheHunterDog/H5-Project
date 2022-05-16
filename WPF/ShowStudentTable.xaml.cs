c7ada23b0d91fc5447964c8f0ddf54ebfd01a1be
LAPTOP-UFABUB1N\trist <s1147577@windesheim.nl>
2022-05-16 09:26:39 +0200

    Feat: detailscherm added

--- WPF/ShowStudentTable.xaml.cs
+++ WPF/ShowStudentTable.xaml.cs
@@ -2,10 +2,12 @@
 using System.Linq;
 using System.Windows;
 using Database.Model;
+using System;
+using System.Diagnostics;
 
 namespace WPF;
-
 public partial class ShowStudentTable : Window
+
 {
     public List<Student> Students { get; set; }
     public ShowStudentTable()
@@ -18,9 +20,15 @@ public partial class ShowStudentTable : Window
         }
 
         Student.ItemsSource = Students;
-
-
     }
 
-     
+    private void selectRow(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        Detailscreen detailscreen = new Detailscreen();
+        Student selectedstudent = (Student)Student.SelectedItem;
+        detailscreen.studentnr = selectedstudent.Studentnummer;
+        detailscreen.addStudentInfo();
+        detailscreen.Show();
+
+    }
 }
