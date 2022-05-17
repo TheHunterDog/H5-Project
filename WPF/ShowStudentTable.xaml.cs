103ee7f310373b3a1b519bdbca9421c69a85eb61
itzantoine <49227434+itzantoine@users.noreply.github.com>
2022-05-17 17:25:16 +0200

    Design studentTable + begeleiders
    Laatst gesproken laat nog data zien van studentbegeleider ga ik morgen aanpassen, zal zo in een andere branch het ontwerp neerzetten

--- WPF/ShowStudentTable.xaml.cs
+++ WPF/ShowStudentTable.xaml.cs
@@ -21,6 +21,4 @@ public partial class ShowStudentTable : Window
 
 
     }
-
-     
 }
---
+++ WPF/ShowStudentTableBegeleider.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace WPF;
+
+public partial class ShowStudentTableBegeleider : Window
+{
+    public ShowStudentTableBegeleider()
+    {
+        InitializeComponent();
+    }
+}
