0710b515120093f8d3b60fec377bf206ec3bdccd
itzantoine <49227434+itzantoine@users.noreply.github.com>
2022-05-25 10:05:12 +0200

    begeleiderstabel eruit gegooid op design branch

--- WPF/ShowStudentTableBegeleider.xaml.cs
+++ WPF/ShowStudentTableBegeleider.xaml.cs
@@ -1,11 +1,13 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using Microsoft.EntityFrameworkCore;
+using WPF.Model;
 
 namespace WPF;
 
 public partial class ShowStudentTableBegeleider : Window
 {
-    public ShowStudentTableBegeleider()
-    {
-        InitializeComponent();
-    }
+    
 }
