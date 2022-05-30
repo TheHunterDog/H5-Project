8fbf3f79f86bc39f82dce8d94821655b2b54f966
itzantoine <49227434+itzantoine@users.noreply.github.com>
2022-05-30 15:44:49 +0200

    new changes functies aangemaakt en pseudocode toegevoegd

--- WPF/Screens/AanwezigheidScherm.xaml.cs
+++ WPF/Screens/AanwezigheidScherm.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using Database.Model;
 
 namespace WPF.Screens;
 
@@ -7,6 +10,36 @@ public partial class AanwezigheidScherm : Window
     public AanwezigheidScherm()
     {
         InitializeComponent();
+    }
+
+    private void showPresenceTable()
+    {
+        using (var context = new StudentBeleidContext())
+        {
+            var presence = context.Students.ToList();
+            PresenceTable.ItemsSource = presence;
+            
+        }
+        
+    }
+
+    private void PresenceboxChecked(object sender, RoutedEventArgs e)
+    {
+        bool aanwezig = AanwezigBox.IsChecked == true;
+
+        if (aanwezig)
+        {
+            // Zet de kolom Present van 0 naar 1 in de database
+
+            using (var context = new StudentBeleidContext())
+            {
+            }
+        }
+
+        if (!aanwezig)
+        {
+            // Zet de kolom Present van 1 naar 0 in de database
+        }
     }
 
     private void SavePresenceBtn(object sender, RoutedEventArgs e)
