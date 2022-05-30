961eedf316c427c4daf5bcb9cc9f2975d67fa130
Mark Heijnekamp <mark@heijnekamp.nl>
2022-05-30 11:45:50 +0200

    Migrations files

---
+++ Database/Migrations/20220530090311_BindSubjecttoStudent.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database.Migrations
+{
+    public partial class BindSubjecttoStudent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentSubjects",
+                columns: table => new
+                {
+                    StudentsId = table.Column<int>(type: "int", nullable: false),
+                    SubjectsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentSubjects", x => new { x.StudentsId, x.SubjectsId });
+                    table.ForeignKey(
+                        name: "FK_StudentSubjects_Students_StudentsId",
+                        column: x => x.StudentsId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StudentSubjects_Subjects_SubjectsId",
+                        column: x => x.SubjectsId,
+                        principalTable: "Subjects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+            
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_StudentBegeleiders_StudentBegeleiderId",
+                table: "Students");
+
+            migrationBuilder.DropTable(
+                name: "StudentSubjects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_StudentBegeleiderId",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "StudentBegeleiderId",
+                table: "Students");
+        }
+    }
+}
