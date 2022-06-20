60742a4668fd1559797933db8915b8c4b541bd4d
Mark Heijnekamp <mark@heijnekamp.nl>
2022-06-20 11:55:52 +0200

    Add Notification.cs class

---
+++ Database/Model/Notification.cs
@@ -0,0 +1,55 @@
+﻿namespace Database.Model;
+
+public class Notification
+{
+    #region PrivateFields
+    /// <summary>
+    /// Notification has an id
+    /// </summary>
+    private int _id;
+    
+    /// <summary>
+    /// Notification has an sender
+    /// </summary>
+    private IAuthenticatable _sender;
+    
+    /// <summary>
+    /// Notification has a receiver
+    /// </summary>
+    private IAuthenticatable _receiver;
+    
+    /// <summary>
+    /// Notification has a Description
+    /// </summary>
+    private string _description;
+    #endregion
+
+    #region properties
+
+    public int Id
+    {
+        get => _id;
+        set => _id = value;
+    }
+
+    public IAuthenticatable Sender
+    {
+        get => _sender;
+        set => _sender = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public IAuthenticatable Receiver
+    {
+        get => _receiver;
+        set => _receiver = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string Description
+    {
+        get => _description;
+        set => _description = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    #endregion
+
+}
