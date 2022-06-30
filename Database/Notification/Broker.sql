70a21836514564037a237d1ffa09cf13b21acd17
Mark Heijnekamp <mark@heijnekamp.nl>
2022-06-30 14:15:41 +0200

    Added SQL file

---
+++ Database/Notification/Broker.sql
@@ -0,0 +1,8 @@
+﻿ALTER DATABASE StudentBegeleid SET ENABLE_BROKER;
+
+CREATE QUEUE NotificationQueue
+CREATE SERVICE NotificationChange ON QUEUE NotificationQueue
+    ([http://schemas.microsoft.com/SQL/Notifications/PostQueryNotification]);
+
+
+GRANT SUBSCRIBE QUERY NOTIFICATIONS TO DATABASE_PRINCIPAL
